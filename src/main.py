122f75f1e80d1b4a720d558d4127a7d90f7c900b
Lucas Campos Vieira <contato@lukerops.com>
2023-06-13 19:46:17 +0000

    adiciona modificações do python

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def soma(x, y):
     return x + y
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("ERRO!! Precisa passar 2 números para somar")
+        exit(1)
+
+    print(soma(int(sys.argv[1]), int(sys.argv[2])))
